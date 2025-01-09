67d0ba39c66c114797fd89bec550c1b8bc047509
Miguel Werneck <mig.roale@gmail.com>
2025-01-09 16:21:48 +0100

    custom query

--- src/main/java/org/example/budgetmanager/repository/ExpensesRepository.java
+++ src/main/java/org/example/budgetmanager/repository/ExpensesRepository.java
@@ -5,6 +5,7 @@ import org.example.budgetmanager.model.Expense;
 import org.example.budgetmanager.model.User;
 import org.example.budgetmanager.repository.entity.ExpensesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -17,5 +18,12 @@ public interface ExpensesRepository extends JpaRepository<ExpensesEntity, Long> 
 
     List<ExpensesEntity> findByUserId(Long userId);
     List<ExpensesEntity> findByUserIdAndDateBetween(Long userId, LocalDate startDate, LocalDate endDate);
-    Collection<ExpensesEntity> findByUserIdAndCategoryAndDateBetween(Long userId, Category category, LocalDate startDate, LocalDate endDate);
+    //Collection<ExpensesEntity> findByUserIdAndCategoryAndDateBetween(Long userId, Category category, LocalDate startDate, LocalDate endDate);
+    @Query("SELECT e FROM ExpensesEntity e WHERE e.userId = :userId AND e.category = :category AND e.date BETWEEN :startDate AND :endDate")
+    Collection<ExpensesEntity> findByUserIdAndCategoryAndDateBetween(
+            Long userId,
+            Category category,
+            LocalDate startDate,
+            LocalDate endDate
+    );
 }
